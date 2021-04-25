a5afffde7e15028b9aa2b6135dd79aa4cd7b6261
Ariele Mendonça <ariele_apm@outlook.com>
2021-04-24 22:25:52 -0300

    Inicio do Ig News

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Inicio | IgNews</title>
+      </Head>
+      <h1>
+        
+        <span>Hello</span> Next
+      </h1>
+    </>
+  )
+}
