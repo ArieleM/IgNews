ffae08495bc762e709b0a0ea76cd6dc01856d07b
Ariele Mendonça <ariele_apm@outlook.com>
2021-04-28 23:19:32 -0300

    Adiciona autenticação com o GitHub

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import {GetServerSideProps} from 'next'
+import {GetStaticProps} from 'next'
 import { SubscribeButton } from '../components/SubscribeButton'
 import styles from './home.module.scss'
 import { stripe } from '../services/stripe'
@@ -20,7 +20,7 @@ export default function Home({product}: HomeProps) {
       </Head>
       <main className={styles.contentContainer}>
         <section className={styles.hero}>
-          <span>👏s Hey, welcome</span>
+          <span>👏 Hey, welcome</span>
           <h1>News about the <span>React</span> world.</h1>
           <p>
             Get access to all the publications <br />
@@ -33,7 +33,7 @@ export default function Home({product}: HomeProps) {
     </>
   )
 }
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve('price_1IkYfVAFToEktt6JNSK24p0u')
   const product = {
     priceId: price.id,
@@ -46,6 +46,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return{
     props: {
       product
-    }
+    },
+    revalidate: 60 * 60 * 24, //24 hours
   }
 }
