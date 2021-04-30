4370b106a14a6956fe212cd75fa5b0515a97d420
Ariele Mendonça <ariele_apm@outlook.com>
2021-04-30 17:33:50 -0300

    Evita duplicação no Stripe

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,5 +1,6 @@
 import { signIn, useSession } from 'next-auth/client'
 import { api } from '../../services/api';
+import { getStripeJs } from '../../services/stripe-js';
 import styles from './styles.module.scss'
 
 interface SubscribeButtonProps{
@@ -15,6 +16,14 @@ export function SubscribeButton({priceId}: SubscribeButtonProps){
     }
     try {
       const response = await api.post('/subscribe')
+
+      const {sessionId} = response.data;
+
+      const stripe = await getStripeJs();
+
+      await stripe.redirectToCheckout({sessionId});
+    }catch(err){
+      alert(err.message);
     }
 
   }
