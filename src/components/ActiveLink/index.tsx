a273cc7e54fd0ec89fbf9d508ec051fb36ba25fe
Ariele Mendonça <ariele_apm@outlook.com>
2021-08-03 22:45:48 -0300

    Remove space

--- src/components/ActiveLink/index.tsx
+++ src/components/ActiveLink/index.tsx
@@ -1,11 +1,13 @@
 import Link, { LinkProps } from "next/link";
-import { ReactElement, cloneElement } from "react";
 import { useRouter } from "next/router";
+
+import { ReactElement, cloneElement } from "react";
 
 interface ActiveLinkProps extends LinkProps {
   children: ReactElement;
   activeClassName: string;
 }
+// clone element do react clona qualquer elemento e adiciona uma propriedade através das opções do mesmo.
 export function ActiveLink({
   children,
   activeClassName,
@@ -13,6 +15,5 @@ export function ActiveLink({
 }: ActiveLinkProps) {
   const { asPath } = useRouter();
   const className = asPath === rest.href ? activeClassName : "";
-
-  return <Link {...rest}> {cloneElement(children, { className })}</Link>;
+  return <Link {...rest}>{cloneElement(children, { className })}</Link>;
 }
